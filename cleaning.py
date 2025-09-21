7875be24e86ac5adcb5291633e52e3a4410db8a5
Rotich Bravin <rotichbravin13@gmail.com>
2025-09-21 09:59:55 +0300

    data cleaning section added

---
+++ cleaning.py
@@ -0,0 +1,23 @@
+# # def myFunct(n):
+# #     global a
+# #     a = int(input("enter the number: "))
+
+# #     return lambda a: a*n
+# # doubler = myFunct(2)
+# # print (doubler(a))
+
+# import numpy as np 
+# array = np.array({'salamu':'hello habari gani?'})
+# print((array.dtype))
+# print(array)
+
+# dict = {'name': 'bravin',
+#         'age': '21',
+#         'education': 'selfInteligence'
+#         }
+# print(dict['name'])
+# print (f"he is based on  {dict['education']}")
+# time = np.array([2005,5,2], dtype = "datetime64[D]")
+# print(time)
+
+#CON
